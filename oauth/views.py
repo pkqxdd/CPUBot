c097db9715f1351cd7c7a86af008b7b67b91b909
Ruocheng Wang <yrrejkk@gmail.com>
2019-09-06 22:16:25 -0400

    Bug fix

--- oauth/views.py
+++ oauth/views.py
@@ -29,7 +29,7 @@ def join(request: HttpRequest):
     if not school_email.strip().endswith('@choate.edu'):
         return HttpResponse("Error: Please provide your Choate email", status=400)
     if Record.objects.filter(school_email=school_email).exists():
-        return HttpResponse("You have already signed up for the club.",status=400)
+        return render(requests,'confirmation_template.html',{"text":"You have already signed up for the club.",'Title':'Error'},status=400)
     try:
         record = Record(first_name=first_name,
                         last_name=last_name,
